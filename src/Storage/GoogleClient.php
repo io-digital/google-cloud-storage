b64bbb5f7bb300e749e0035f43cbebbc8b8a09b4
Ian van der Merwe <ian@io.co.za>
2016-03-14 14:05:07 +0200

    changed config details layout

--- src/Storage/GoogleClient.php
+++ src/Storage/GoogleClient.php
@@ -2,38 +2,60 @@
 
 class GoogleClient
 {
+    /**
+     * @var \Google_Client
+     * Google Client Instance
+     */
     protected $client;
+    /**
+     * @var string
+     * Project Name
+     */
     protected $projectName;
+    /**
+     * @var string
+     * Bucket name
+     */
     protected $projectBucket;
 
     /**
      * GoogleServiceProvider constructor.
-     *
+     * @param $config
      */
     public function __construct()
     {
         $this->client = new \Google_Client();
 
-        $this->client->setApplicationName(config('google_cloud_storage.application'));
-
-        $this->client->setAuthConfigFile(base_path(config('google_cloud_storage.auth_json_filename')));
-
-        $this->client->addScope(\Google_Service_Storage::DEVSTORAGE_FULL_CONTROL);
-
         $this->projectName = config('google_cloud_storage.application');
         $this->projectBucket = config('google_cloud_storage.bucket');
+        $this->authJsonFile = config('google_cloud_storage.auth_json_filename');
+
+        $this->client->setApplicationName($this->projectName);
+
+        $this->client->setAuthConfigFile(base_path($this->authJsonFile));
+
+        $this->client->addScope(\Google_Service_Storage::DEVSTORAGE_FULL_CONTROL);
     }
 
+    /**
+     * @return \Google_Client
+     */
     public function getClient()
     {
         return $this->client;
     }
 
+    /**
+     * @return string
+     */
     public function getApplicationName()
     {
         return $this->projectName;
     }
 
+    /**
+     * @return string
+     */
     public function getBucketName()
     {
         return $this->projectBucket;
